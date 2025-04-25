18c6ffa015075389398fcbf5a8cba674ae215870
Anatolii Krotov <anatolii.krotov@gmail.com>
2025-04-25 11:54:16 -0400

    Added new tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -30,6 +30,7 @@ from decimal import Decimal
 from service.models import Product, Category, db
 from service import app
 from tests.factories import ProductFactory
+from service.models import DataValidationError
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
@@ -130,6 +131,16 @@ class TestProductModel(unittest.TestCase):
         self.assertEquals(products[0].id, original_id)
         self.assertEquals(products[0].description, "testing")
 
+    def test_update_a_product_with_none_id(self):
+        """It should update a product with none id"""
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+        product.id = None
+        with self.assertRaises(DataValidationError):
+            product.update()
+
     def test_delete_a_product(self):
         """It should delete a product"""
         product = ProductFactory()
@@ -185,3 +196,16 @@ class TestProductModel(unittest.TestCase):
         self.assertEquals(found.count(), count)
         for product in found:
             self.assertEquals(product.category, category)
+
+    def test_find_a_product_by_price(self):
+        """It should find a product by price"""
+        products = ProductFactory.create_batch(5)
+        for product in products:
+            product.create()
+        price = products[0].price
+        count = len([product for product in products if product.price == price])
+        found = Product.find_by_price(price)
+        self.assertEquals(found.count(), count)
+        for product in found:
+            self.assertEquals(product.price, price)
+
